1485eba6ee64d5c206f931cb349957b27a1f04f5
Florian Thoma <florian.thoma@innoweb.com.au>
2016-01-30 20:57:54 +1100

    initial commit

---
+++ code/InternationalPhoneNumberField.php
@@ -0,0 +1,78 @@
+<?php
+
+class InternationalPhoneNumberField extends TextField {
+	
+	public function __construct($name, $title = null, $value = '') {
+		parent::__construct($name, $title, $value);
+		$this->addExtraClass('InternationalPhoneNumberField');
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function Type() {
+		return 'tel text';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getAttributes() {
+		return array_merge(
+			parent::getAttributes(),
+			array(
+				'type' => 'tel',
+			)
+		);
+	}
+	
+	public function FieldHolder($properties = array()) {
+
+		// load requirements
+		Requirements::css('international-phone-number-field/lib/intl-tel-input/build/css/intlTelInput.css');
+		Requirements::javascript(THIRDPARTY_DIR.'/jquery/jquery.min.js');
+		Requirements::javascript(THIRDPARTY_DIR.'/jquery-validate/jquery.validate.min.js');
+		Requirements::combine_files(
+			'InternationalPhoneNumberField.js',
+			array(
+				'international-phone-number-field/lib/intl-tel-input/build/js/intlTelInput.min.js',
+				'international-phone-number-field/javascript/init.js',
+			)
+		);
+		
+		// call parent
+		$html = parent::FieldHolder();
+		return $html;
+	}
+	
+	public function setValue($val) {
+		if(empty($val)) {
+			$this->value = null;
+		} else {
+			$phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
+			try {
+				$numberProto = $phoneUtil->parse($value);
+				if ($phoneUtil->isValidNumber($numberProto)) {
+					$this->value = $phoneUtil->format($numberProto, \libphonenumber\PhoneNumberFormat::INTERNATIONAL);
+				}
+			} catch (\libphonenumber\NumberParseException $e) {
+				$this->value = null;
+			}
+		}
+		return $this;
+	}
+	
+	
+	public function validate($validator) {
+		$phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
+		try {
+			$numberProto = $phoneUtil->parse($this->value);
+			if ($phoneUtil->isValidNumber($numberProto)) {
+				return true;
+			}
+		} catch (\libphonenumber\NumberParseException $e) {
+			return false;
+		}
+		return false;
+	}
+}
---
+++ javascript/init.js
@@ -0,0 +1,33 @@
+(function($) {
+	$(document).ready(function() {
+		
+		// add validator method
+		$.validator.addMethod("internationalPhone", function(phone_number, element) {
+			return this.optional(element) 
+				|| $(element).intlTelInput("isValidNumber");
+		}, "Please enter a correct phone number.");
+
+		// init phone field
+		$(".field.InternationalPhoneNumberField input").intlTelInput({
+			// allowExtensions: true,
+			// autoFormat: false,
+			// autoHideDialCode: false,
+			// autoPlaceholder: false,
+			// dropdownContainer: $("body"),
+			//excludeCountries: ["us"],
+			geoIpLookup: function(callback) {
+				$.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+					var countryCode = (resp && resp.country) ? resp.country : "";
+					callback(countryCode);
+				});
+			 },
+			initialCountry: "auto",
+			// nationalMode: false,
+			// numberType: "MOBILE",
+			// onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+			preferredCountries: ['au', 'nz'],
+			utilsScript: "/international-phone-number-field/lib/intl-tel-input/lib/libphonenumber/build/utils.js"
+		});
+
+	});
+}(jQuery));
