c621198d35b3312eae55082b49cab0ed969ca999
Florian Thoma <florian.thoma@innoweb.com.au>
2019-06-24 12:36:35 +1000

    ugrade frontend build to gulp 4

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,20 +1,18 @@
-// load modules
-var autoprefixer	= require('gulp-autoprefixer'),
-	gulp 			= require('gulp'),
-	del 			= require('del'),
-	path 			= require('path'),
-	plumber 		= require('gulp-plumber'),
-	cleancss 		= require('gulp-clean-css'),
-	concat 			= require('gulp-concat-util'),
-	sass 			= require('gulp-sass'),
-	cssimport 		= require("gulp-cssimport"),
-	stripDebug 		= require('gulp-strip-debug'),
-	uglify 			= require('gulp-uglify'),
-	sourcemaps 		= require('gulp-sourcemaps'),
-	stripdebug 		= require('gulp-strip-debug');
+const { src, dest, watch, series, parallel } = require('gulp');
+const del = require('del');
+const path = require('path');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const cssimport = require('gulp-cssimport');
+const concat = require('gulp-concat-util');
+const stripdebug = require('gulp-strip-debug');
+const uglify = require('gulp-uglify');
 
 // load paths
-var paths = {
+const paths = {
 	"src": "./src/",
 	"dist": "./dist/",
 	"absoluteBase": "/resources/vendor/innoweb/silverstripe-international-phone-number-field",
@@ -36,7 +34,7 @@ var paths = {
 	}
 };
 
-var scriptFiles = {
+const scriptFiles = {
 	"utils.js": [
 		"node_modules/intl-tel-input/build/js/utils.js"
 	],
@@ -49,46 +47,45 @@ var scriptFiles = {
 	]
 }
 
-
-var sassOptions = {
+const sassOptions = {
     errLogToConsole: true,
     outputStyle: 'compressed'
 };
 
-var autoprefixerOptions = {
+const autoprefixerOptions = {
     browsers: ['last 2 versions', '> 1%', 'IE >= 9'],
     cascade: false,
     supports: false
 };
 
-gulp.task('styles', ['cleanstyles'], function () {
-    return gulp
-        .src(paths.src + paths.styles.src + paths.styles.filter)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(cssimport({matchPattern: "*.css"}))
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(cleancss({processImport: true, keepSpecialComments: 0}))
-        .pipe(sourcemaps.write('.', {
-            sourceMappingURLPrefix: paths.absoluteBase + '/' + paths.styles.dist
-        }))
-        .pipe(gulp.dest(paths.dist + paths.styles.dist));
-});
+function styles(cb) {
+	return src(paths.src + paths.styles.src + paths.styles.filter)
+	    .pipe(plumber({
+	        errorHandler: onError
+	    }))
+	    .pipe(sourcemaps.init())
+	    .pipe(cssimport({matchPattern: "*.css"}))
+	    .pipe(sass(sassOptions).on('error', sass.logError))
+	    .pipe(autoprefixer(autoprefixerOptions))
+	    .pipe(cleancss({processImport: true, keepSpecialComments: 0}))
+	    .pipe(sourcemaps.write('.', {
+	        sourceMappingURLPrefix: paths.absoluteBase + '/' + paths.styles.dist
+	    }))
+	    .pipe(dest(paths.dist + paths.styles.dist));
+	cb();
+}
 
-gulp.task('cleanstyles', function() {
-    return del.sync([
+function cleanStyles(cb) {
+    return del([
     	paths.dist + paths.styles.dist
     ]);
-});
+	cb();
+}
 
-gulp.task('scripts', ['cleanscripts'], function() {
-    var scriptNames = Object.keys(scriptFiles);
-    scriptNames.forEach(function(scriptName) {
-        return gulp
-            .src(
+function scripts(cb) {
+	var scriptNames = Object.keys(scriptFiles);
+	scriptNames.forEach(function(scriptName) {
+		return src(
                 scriptFiles[scriptName],
                 {
                     cwd: path.join(process.cwd(), './'),
@@ -100,45 +97,75 @@ gulp.task('scripts', ['cleanscripts'], function() {
             }))
             .pipe(sourcemaps.init())
             .pipe(concat(scriptName))
-//            .pipe(stripdebug())
+            .pipe(stripdebug())
             .pipe(uglify({mangle: false}))
             .pipe(sourcemaps.write('.', {
                 sourceMappingURLPrefix: paths.absoluteBase + '/' + paths.scripts.dist
             }))
-            .pipe(gulp.dest(paths.dist + paths.scripts.dist));
-    });
-});
+            .pipe(dest(paths.dist + paths.scripts.dist));
+	});
+	cb();
+}
 
-gulp.task('cleanscripts', function() {
-    return del.sync([
-    	paths.dist + paths.scripts.dist
-    ]);
-});
+function cleanScripts(cb) {
+	return del([
+		paths.dist + paths.scripts.dist
+	]);
+	cb();
+}
 
-gulp.task('images', ['cleanimages'], function(){
-    return gulp
-        .src(paths.images.src + paths.images.filter)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(gulp.dest(paths.dist + paths.images.dist))
-});
+function images(cb) {
+	return src(paths.images.src + paths.images.filter)
+	    .pipe(plumber({
+	        errorHandler: onError
+	    }))
+	    .pipe(dest(paths.dist + paths.images.dist));
+	cb();
+}
 
-gulp.task('cleanimages', function() {
-    return del.sync([
-    	paths.dist + paths.images.dist + paths.images.filter
-    ]);
-});
+function cleanImages(cb) {
+	return del([
+		paths.dist + paths.images.dist + paths.images.filter
+	]);
+	cb();
+}
 
-gulp.task('watch', function() {
-	gulp.watch(paths.src + paths.styles.src + paths.styles.filter, ['styles']);
-	gulp.watch(paths.src + paths.scripts.src + paths.scripts.filter, ['scripts']);
-});
+function watchAll() {
+	// watch for style changes
+	watch(paths.src + paths.styles.src + paths.styles.filter, series(cleanStyles, styles));
+	// watch for script changes
+	watch(paths.src + paths.scripts.src + paths.scripts.filter, series(cleanScripts, scripts));
+	// watch for image changes
+	watch(paths.images.src + paths.images.filter, series(cleanImages, images));
+}
 
-gulp.task('default', ['styles', 'scripts', 'images']);
-
-gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
-
-var onError = function(err) {
+function onError(err) {
     console.log(err);
 }
+
+exports.build = series(
+	parallel(
+		cleanStyles,
+		cleanScripts,
+		cleanImages
+	),
+	parallel(
+		styles,
+		scripts,
+		images
+	)
+);
+
+exports.default = series(
+	parallel(
+		cleanStyles,
+		cleanScripts,
+		cleanImages
+	),
+	parallel(
+		styles,
+		scripts,
+		images
+	),
+	watchAll
+);
