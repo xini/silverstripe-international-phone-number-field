d0eeff0eb8dfd882115deb37dffc4e4b1d76564e
Florian Thoma <florian.thoma@innoweb.com.au>
2021-10-16 00:04:26 +1100

    switch to national mode for better usability, use hidden field to submit complete phone number to the server

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [4.2.0]
+
+* switch to national mode for better usability, use hidden field to submit complete phone number to the server
+
 ## [4.1.0]
 
 * add support for Abstract, ipgeolocation and freegeoip
--- client/src/javascript/field-init.js
+++ client/src/javascript/field-init.js
@@ -10,51 +10,76 @@
 			
 			// define geo lookup function
 			var geoLookup = null;
-	        var initialCountry = field.getAttribute('data-initialcountry');
-	        if (
-	            field.getAttribute('data-apiurl') && field.getAttribute('data-apiurl').length > 0
-	            && (typeof initialCountry === 'undefined' || initialCountry === 'auto')
-	        ) {
+			var initialCountry = field.getAttribute('data-initialcountry');
+			if (
+				field.getAttribute('data-apiurl') && field.getAttribute('data-apiurl').length > 0
+				&& (typeof initialCountry === 'undefined' || initialCountry === 'auto')
+			) {
 				geoLookup = function(callback) {
 					var xhr = new XMLHttpRequest();
 					xhr.open('GET', field.getAttribute('data-apiurl'));
 					xhr.setRequestHeader("Accept", "application/json");
 					xhr.onload = function() {
-					    if (xhr.status === 200) {
-					    	var json = JSON.parse(xhr.responseText);
-					    	var countryCode = (json && json[field.getAttribute('data-apireplykey')]) ? json[field.getAttribute('data-apireplykey')] : "";
+						if (xhr.status === 200) {
+							var json = JSON.parse(xhr.responseText);
+							var countryCode = (json && json[field.getAttribute('data-apireplykey')]) ? json[field.getAttribute('data-apireplykey')] : "";
 							callback(countryCode);
-					    }
+						}
 					};
 					xhr.send();
 				};
 			}
-		
-			window.intlTelInput(field, {
+			
+			// create hidden field for data submission
+			var fieldname = field.getAttribute('name');
+			var hiddenField = document.createElement("input");
+			hiddenField.setAttribute('type', 'hidden');
+			hiddenField.setAttribute('name', fieldname);
+			hiddenField.setAttribute('value', field.value);
+			// rename visible field
+			field.setAttribute('name', fieldname + '_itl');
+			// insert hidden field
+			field.parentElement.appendChild(hiddenField);
+			
+			var iti = window.intlTelInput(field, {
 				geoIpLookup: geoLookup,
 				initialCountry: initialCountry,
-				nationalMode: false,
+				nationalMode: true,
 				onlyCountries: field.getAttribute('data-onlycountries') ? field.getAttribute('data-onlycountries').split('-') : [],
 				preferredCountries: field.getAttribute('data-preferredcountries') ? field.getAttribute('data-preferredcountries').split('-') : [],
 				excludeCountries: field.getAttribute('data-excludedcountries') ? field.getAttribute('data-excludedcountries').split('-') : [],
 				utilsScript: field.getAttribute('data-utilsscripturl'),
 			});
+			
+			// Update hidden field value with phone number if correct, otherwise just copy value. Validation happens on server.
+			var handleChange = function() {
+				if (field.value.trim()) {
+					if (iti.isValidNumber()) {
+						hiddenField.value = iti.getNumber();
+					} else {
+						hiddenField.value = field.value;
+					}
+				}
+			};
+			field.addEventListener('change', handleChange);
+			field.addEventListener('keyup', handleChange);
+			field.addEventListener('blur', handleChange);
 		});
 	}
 
-	if (document.readyState === "loading") {  // Loading hasn't finished yet
+	if (document.readyState === "loading") { // Loading hasn't finished yet
 		document.addEventListener("DOMContentLoaded", initInternationalPhoneField);
-	} else {  // `DOMContentLoaded` has already fired
+	} else { // `DOMContentLoaded` has already fired
 		initInternationalPhoneField();
 	}
 	
 	if (window.jQuery && window.jQuery.fn.entwine) { 
 		jQuery.entwine("InternationalPhoneNumberField", function ($) {
-		    $(":input.InternationalPhoneNumberField").entwine({
-		        onmatch: function () {
-		            initInternationalPhoneField();
-		        }
-		    });
+			$(":input.InternationalPhoneNumberField").entwine({
+				onmatch: function () {
+					initInternationalPhoneField();
+				}
+			});
 		});
 	}
 
